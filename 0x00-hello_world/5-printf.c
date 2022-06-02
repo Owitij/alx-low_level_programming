18efcab35caa06189d1db87c62e51b8dcc54f278
Owitij <judith.owitti@gmail.com>
2022-06-02 20:03:42 +0300

    Prints with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Beginning of function
+ *
+ * Return 0. It returns nothing.
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
