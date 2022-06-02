d2d84f8e380b58b3bd6fe1db471adbc30326b318
Owitij <judith.owitti@gmail.com>
2022-06-02 20:21:23 +0300

    with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Beginning of function
+ * main - Entry point
  *
- * Return 0. It returns nothing.
+ * Return: return 0
  */
 int main(void)
 {	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
