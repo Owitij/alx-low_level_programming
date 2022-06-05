009190bf97db5f97c10b2997e9ad8d042b18e315
Owitij <judith.owitti@gmail.com>
2022-06-05 08:47:56 -0700

    print lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 Success
+ */
+int main(void)
+{
+char lower = 'a';
+for (lower = 'a'; lower <= 'z'; lower++)
+putchar(lower);
+return (0);
+}
