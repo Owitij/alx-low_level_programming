7743afcae5e821e915106178c6120045d496fa3d
Owitij <judith.owitti@gmail.com>
2022-06-08 08:50:23 -0700

    adding two variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - two integers and returns the result
+ *@a: first input
+ *@b: second input
+ *Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
